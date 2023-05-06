ffc77cb9c9f40a212ae185e93a5351f76447f55e
AlinaSau <saushkina_lina@mail.ru>
2023-05-06 17:24:57 +0300

    some changes

--- js/data.js
+++ js/data.js
@@ -1,16 +1,16 @@
-import { getRandInt } from "./util.js";
+import { getRandInt } from './util.js';
 function getPhotos() {
-    const photo = [];
-    for (let i=0; i<25; i++) {
-        photo.push({
-            id: i+1,
-            url: `photo/${i+1}.jpg`,
-            description: `my favourite photo #${i+1}`,
-            likes: getRandInt(15, 200),
-            comments: getRandInt(0, 200)
-        })
+  const photo = [];
+  for (let i=0; i<25; i++) {
+      photo.push({
+        id: i+1,
+        url: `photo/${i+1}.jpg`,
+        description: `my favourite photo #${i+1}`,
+        likes: getRandInt(15, 200),
+        comments: getRandInt(0, 200)
+      })
     }
-    return photo;
+  return photo;
 }
 
 export {getPhotos};
