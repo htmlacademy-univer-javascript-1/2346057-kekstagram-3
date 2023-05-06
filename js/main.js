81c746b17b01344915715d6c292784703fc9a0aa
AlinaSau <saushkina_lina@mail.ru>
2023-05-06 18:21:13 +0300

    module7-task1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-import { getRandInt, isStringFits, getPhotos } from './utils.js';
-
-getRandInt(0, 200);
-isStringFits('', 12);
-getPhotos();
+import './data.js';
+import './util.js';
+import './rend.js';
